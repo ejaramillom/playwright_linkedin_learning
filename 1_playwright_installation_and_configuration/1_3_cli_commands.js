c487948237bae627932b3fbfe611c0f02e3b22ea
ejaramillom <ejaramilo@hotmail.com>
2025-09-25 16:05:45 +0000

    [PLW-004] feat:  additional commands for tagged groups of tests

--- 1_playwright_installation_and_configuration/1_3_cli_commands.js
+++ 1_playwright_installation_and_configuration/1_3_cli_commands.js
@@ -13,3 +13,14 @@ npx playwright test --project chromium --project firefox
 // run specific test
 
 npx playwright test tests/example/my_test
+
+// run specifically tagged tests
+
+// test file
+test('get started link', { tag: '@first' }, async ({ page }) => {})
+
+// then in CLI
+npx playwright test --grep @first 
+
+// or run all but the first tagged
+npx playwright test --grep-invert @first  
